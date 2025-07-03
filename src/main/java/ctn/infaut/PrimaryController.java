00d56079b6e7b307b0d8c95484300ddc212be7e9
Isaac Feltes <isaacfg2812@gmail.com>
2025-07-03 13:11:35 -0300

    Agregar carpeta nueva al readme
    Esta carpeta tiene datos confidenciales, te voy a pasr por whatsapp

--- src/main/java/ctn/infaut/PrimaryController.java
+++ src/main/java/ctn/infaut/PrimaryController.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
-
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+  @FXML
+  private void switchToSecondary() throws IOException {
+    App.setRoot("secondary");
+  }
 }
