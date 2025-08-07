25f459d5c0ea75e29e9f4c52f11fd0c1c1d40d28
Isaac Feltes <isaacfg2812@gmail.com>
2025-08-07 16:15:33 -0300

    feat: Agregar controlador para los docentes
    en un futuro voy a separar la logica de sql a un service

--- src/main/java/ctn/infaut/controllers/Docente.java
+++ src/main/java/ctn/infaut/controllers/Docente.java
@@ -1,11 +1,17 @@
 package ctn.infaut.controllers;
 
+import java.util.ArrayList;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 import ctn.infaut.connection.Conexion;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.sql.Statement;
+
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class Docente implements Conexion {
 
@@ -14,22 +20,7 @@ public class Docente implements Conexion {
   private String apellido;
   private int numeroCI;
 
-  @Override
-  public boolean insertar() {
-    String sql = "INSERT INTO infaut.Docente (nombre, apellido CI) VALUES (?, ?, ?)";
-    try (PreparedStatement pstmt = Conexion.getCon().prepareStatement(sql)) {
-      pstmt.setString(1, this.nombre);
-      pstmt.setString(2, this.apellido);
-      pstmt.setInt(3, this.numeroCI);
-
-      pstmt.executeUpdate();
-
-      return true;
-
-    } catch (SQLException e) {
-      Logger.getLogger(Docente.class.getName()).log(Level.SEVERE, null, e);
-      return false;
-    }
+  public Docente() {
   }
 
   public Docente(int idDocente, String nombre, String apellido, int numeroCI) {
@@ -39,33 +30,99 @@ public class Docente implements Conexion {
     this.numeroCI = numeroCI;
   }
 
-  public Docente() {
+  public Docente(String nombre, String apellido, int numeroCI) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.numeroCI = numeroCI;
+  }
+
+  // TODO: Mover los metodos relacionados al SQL en el service correspondiente
+
+  @Override
+  public boolean insertar() {
+    String sql = "INSERT INTO infaut.docente (nombre, apellido, ci) VALUES (?, ?, ?)";
+    try (PreparedStatement pstmt = Conexion.getCon().prepareStatement(sql)) {
+      pstmt.setString(1, this.nombre);
+      pstmt.setString(2, this.apellido);
+      pstmt.setInt(3, this.numeroCI);
+
+      pstmt.executeUpdate();
+
+      return true;
+
+    } catch (SQLException ex) {
+      Logger.getLogger(Docente.class.getName()).log(Level.SEVERE, null, ex);
+      return false;
+    }
   }
 
   @Override
   public boolean modificar() {
-    String sql = "UPDATE * FROM infaut.docente SET, nombre = ?, apellido = ?, ci = ? WHERE iddocente = ?";
+    String sql = "UPDATE infaut.docente SET nombre = ?, apellido = ?, ci = ? WHERE id_docente = ?";
 
     try (PreparedStatement pstmt = Conexion.getCon().prepareStatement(sql)) {
 
       pstmt.setString(1, this.nombre);
       pstmt.setString(2, this.apellido);
       pstmt.setInt(3, this.numeroCI);
-      pstmt.setInt(4, idDocente);
+      pstmt.setInt(4, this.idDocente);
+
+      pstmt.executeUpdate();
 
       return true;
     } catch (SQLException ex) {
+      Logger.getLogger(Docente.class.getName()).log(Level.SEVERE, null, ex);
       return false;
     }
   }
 
   @Override
   public boolean eliminar() {
-    return false;
+    String sql = "DELETE FROM infaut.docente WHERE id_docente=?";
+
+    try (PreparedStatement pstmt = Conexion.getCon().prepareStatement(sql)) {
+      pstmt.setInt(1, this.idDocente);
+      pstmt.executeUpdate();
+      return true;
+    } catch (SQLException ex) {
+      Logger.getLogger(Docente.class.getName()).log(Level.SEVERE, null, ex);
+      return false;
+    }
+  }
+
+  public ArrayList<Docente> consulta() throws SQLException {
+    ArrayList<Docente> teachers = new ArrayList<Docente>();
+    try (Statement stmt = Conexion.getCon().createStatement()) {
+      String sql = "SELECT * FROM infaut.docente WHERE 1=1";
+      ResultSet rs = stmt.executeQuery(sql);
+      while (rs.next()) {
+
+        teachers.add(
+            new Docente(
+                rs.getInt("id_docente"),
+                rs.getString("nombre"),
+                rs.getString("apellido"),
+                rs.getInt("ci")));
+      }
+    } catch (SQLException ex) {
+      System.err.println("Hubo un error al obtener los docentes: ");
+      Logger.getLogger(Docente.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }
+
+    return teachers;
+  }
+
+  public int getIdDocente() {
+    return this.idDocente;
+  }
+
+  public void setIdDocente(int idDocente) {
+    this.idDocente = idDocente;
   }
 
   public String getNombre() {
-    return nombre;
+    return this.nombre;
   }
 
   public void setNombre(String nombre) {
@@ -73,18 +130,18 @@ public class Docente implements Conexion {
   }
 
   public String getApellido() {
-    return apellido;
+    return this.apellido;
   }
 
   public void setApellido(String apellido) {
     this.apellido = apellido;
   }
 
-  public int getNumeroCI() {
+  public int getCI() {
     return numeroCI;
   }
 
-  public void setNumeroCI(int numeroCI) {
+  public void setCI(int numeroCI) {
     this.numeroCI = numeroCI;
   }
 }
