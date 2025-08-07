2a22630c795ff3414f5ce72d5f030b678950445a
Isaac Feltes <isaacfg2812@gmail.com>
2025-08-07 16:15:01 -0300

    feat: Agregar interfaz para la conexion

--- src/main/java/ctn/infaut/connection/Conexion.java
+++ src/main/java/ctn/infaut/connection/Conexion.java
@@ -9,26 +9,28 @@ import java.util.Properties;
 
 public interface Conexion {
 
-	public boolean insertar();
-	public boolean modificar();
-	public boolean eliminar();
+  public boolean insertar();
 
-	public static Connection getCon() throws SQLException {
-		Properties props = new Properties();
-		try (FileInputStream db = new FileInputStream("db.properties")) {
-			// NOTE: Fijate lo que te puse en el .gitignore
-			props.load(db);
-			String url = props.getProperty("db.url");
+  public boolean modificar();
 
-			Connection con = DriverManager.getConnection(url);
-			System.out.println("Se conectó con éxito a la base de datos, yay");
-			return con;
-		} catch (SQLException e) {
-			System.err.println("Error al conectar con la base de datos: " + e.getMessage());
-			return null;
-		} catch (IOException fe) {
-			System.err.println("Error al leer las credenciales de la base de datos:" + fe.getMessage());
-			return null;
-		}
-	}
+  public boolean eliminar();
+
+  public static Connection getCon() throws SQLException {
+    Properties props = new Properties();
+    try (FileInputStream db = new FileInputStream("db.properties")) {
+      // NOTE: Fijate lo que te puse en el .gitignore
+      props.load(db);
+      String url = props.getProperty("db.url");
+
+      Connection con = DriverManager.getConnection(url);
+      System.out.println("Se conectó con éxito a la base de datos, yay");
+      return con;
+    } catch (SQLException e) {
+      System.err.println("Error al conectar con la base de datos: " + e.getMessage());
+      return null;
+    } catch (IOException fe) {
+      System.err.println("Error al leer las credenciales de la base de datos:" + fe.getMessage());
+      return null;
+    }
+  }
 }
