2b1a4bfedcd7ba8ac9f2264e48fd5d480a1fe519
Isaac Feltes <isaacfg2812@gmail.com>
2025-08-14 10:11:09 -0300

    feat: Agregar clases para materia

---
+++ src/main/java/ctn/infaut/controllers/Materia.java
@@ -0,0 +1,47 @@
+package ctn.infaut.controllers;
+
+public class Materia {
+  Integer idMateria;
+  String nombre;
+  String horaInicio;
+  String horaFin;
+  Integer idAula;
+
+  public Integer getIdMateria() {
+    return this.idMateria;
+  }
+
+  public String getNombre() {
+    return this.nombre;
+  }
+
+  public Integer getIdAula() {
+    return this.idAula;
+  }
+
+  public String getHoraInicio() {
+    return this.horaInicio;
+  }
+
+  public String getHoraFin() {
+    return this.horaFin;
+  }
+
+  public void setIdMateria(Integer id) {
+    this.idMateria = id;
+  }
+
+  public void setIdAula(Integer id) {
+    this.idAula = id;
+  }
+
+  public Materia() {
+  }
+
+  public Materia(Integer im, String n, String hi, String hf) {
+    this.idMateria = im;
+    this.nombre = n;
+    this.horaInicio = hi;
+    this.horaFin = hf;
+  }
+}
