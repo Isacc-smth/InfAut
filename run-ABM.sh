8b81e367feb47249f152bc07d95bf0c4d3cc4207
Isaac Feltes <isaacfg2812@gmail.com>
2025-08-27 10:37:47 -0300

    feat: Agregar controlador para el Menu de Alumnos

--- run-ABM.sh
+++ run-ABM.sh
@@ -3,8 +3,14 @@
 # Exit immediately if any command fails
 set -e
 
+export GDK_BACKEND=x11
+
 # Clean and compile
 mvn clean compile
 
+# Force X11 + GTK2
 # Run the application
-mvn exec:java -Dexec.mainClass="ctn.infaut.App"
+mvn exec:java \
+  -Dexec.mainClass="ctn.infaut.App" \
+  -Dexec.jvmArgs="-Djdk.gtk.version=2 -Dprism.verbose=true"
+
