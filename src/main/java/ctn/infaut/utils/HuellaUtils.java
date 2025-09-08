94f9c45cb44ff8a46d0b6701770f6f7d6e1c3b5d
Isaac Feltes <isaacfg2812@gmail.com>
2025-09-08 01:45:46 -0300

    docs: Agregar @return faltante a HuellaUtils

--- src/main/java/ctn/infaut/utils/HuellaUtils.java
+++ src/main/java/ctn/infaut/utils/HuellaUtils.java
@@ -11,18 +11,18 @@ import com.machinezoo.sourceafis.FingerprintTemplate;
  * Clase que contiene utilidades para obtener y serializar
  * huellas
  *
- *
  * @author isaacfeltes
- *
  * */
 public interface HuellaUtils {
     /** 
      * Método para generar una imagen comprimida y serilizada desde
-     * un archivo. Solamente utilizar para pruebas; en la práctica,
-     * deberá obtenerlas mediante el SDK del dispositivo 
+     * un archivo, solamente utilizado en pruebas. En la práctica,
+     * deberá obtenerlas mediante el SDK del dispositivo o algún protocolo
+     * como el RS-485 que fue mi caso.
      *
      * @param path La ruta del archivo
      *
+     * @return Una huella serializada lista para usarse
      * */
     public static byte[] fromPath(String path) {
         try {
