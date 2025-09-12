25bdacabc31b61504fa8d88d4b1a80f03f03dd65
Isaac Feltes <isaacfg2812@gmail.com>
2025-09-12 20:53:45 -0300

    feat: Primera version del DAO para unir cursos con materias

--- src/main/java/ctn/infaut/DTO/CursoMateriaDTO.java
+++ src/main/java/ctn/infaut/DTO/CursoMateriaDTO.java
@@ -2,7 +2,9 @@ package ctn.infaut.DTO;
 
 public class CursoMateriaDTO {
    private Integer idCurso; 
+   private String curso;
    private Integer idMateria;
+   private String materia;
 
    // Getters
    public Integer getIdCurso() { return idCurso; }
@@ -12,5 +14,15 @@ public class CursoMateriaDTO {
    public void setIdCurso(Integer idCurso) { this.idCurso = idCurso; }
    public void setIdMateria(Integer idMateria) { this.idMateria = idMateria; }
 
+   public CursoMateriaDTO(Integer idCurso, String curso, Integer idMateria, String materia) {
+       this.idCurso = idCurso;
+       this.curso = curso;
+       this.idMateria = idMateria;
+       this.materia = materia;
+   }
 
+   public CursoMateriaDTO(Integer idCurso, String materia) {
+       this.idCurso = idCurso;
+       this.materia = materia;
+   }
 }
