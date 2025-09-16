a282ddbee4e286c1bd785b599bc8ea8bfb618eef
Isaac Feltes <isaacfg2812@gmail.com>
2025-09-16 08:04:02 -0300

    refactor: Agregar tabla asistencia

--- crear_DB.sql
+++ crear_DB.sql
@@ -41,7 +41,6 @@ create table infaut.alumno (
 CREATE TABLE asistencia (
     id_asistencia SERIAL PRIMARY KEY,
     id_alumno INT NOT NULL,
-    id_aula INT,
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_alumno) REFERENCES alumno(id_alumno),
     FOREIGN KEY (id_aula) REFERENCES aula(id_aula)
