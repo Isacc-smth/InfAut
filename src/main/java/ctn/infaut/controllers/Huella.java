a2f3a20de11ab65a6fb8eda66e85e927233db67c
Isaac Feltes <isaacfg2812@gmail.com>
2025-09-16 07:57:43 -0300

    feat: Insercion de huellas

--- src/main/java/ctn/infaut/controllers/Huella.java
+++ src/main/java/ctn/infaut/controllers/Huella.java
@@ -1,34 +1,49 @@
 package ctn.infaut.controllers;
 
+import com.machinezoo.sourceafis.FingerprintImage;
+import com.machinezoo.sourceafis.FingerprintTemplate;
+
 import ctn.infaut.huella.HuellaUtils;
 
 public class Huella {
     private Integer idHuella;
     private Integer idAlumno;
-    private byte[] imagenHuella;
+    private FingerprintTemplate imagen;
+    private byte[] serializada;
 
-    public Huella(Integer idHuella, Integer idAlumno, String pathHuella) {
-        this.idHuella = idHuella;
+    // Instanciar huella desde el path de una imagen seleccionada 
+    public Huella( Integer idAlumno, String pathHuella) {
         this.idAlumno = idAlumno;
 
         // No quiero reescribir el metodo para obtener la huella, porque es larguito
-        this.imagenHuella = HuellaUtils.fromPath(pathHuella);
+        this.imagen = HuellaUtils.obtenerTemplateDesdePath(pathHuella);
+        this.serializada = HuellaUtils.serializarDesdePath(pathHuella);
     }
 
+    // Instanciar huella desde el path de una imagen seleccionada
+        public Huella(Integer idHuella, Integer idAlumno, String pathHuella, String horaEntrada) {
+            this.idHuella = idHuella;
+            this.idAlumno = idAlumno;
+            this.imagen = HuellaUtils.obtenerTemplateDesdePath(pathHuella);
+            this.serializada = HuellaUtils.serializarDesdePath(pathHuella);
+        }
+
     // La base de datos retorna un array de bytes
-    public Huella(Integer idHuella, Integer idAlumno, byte[] huella) {
+    public Huella(Integer idHuella, byte[] huella, Integer idAlumno) {
         this.idAlumno = idAlumno;
         this.idHuella = idHuella;
-        this.imagenHuella = huella;
+        this.serializada = huella;
     } 
 
     // Getters
     public Integer getIdHuella() { return idHuella; }
     public Integer getIdAlumno() { return idAlumno; }
-    public byte[] getImagenHuella() { return imagenHuella; }
+    public FingerprintTemplate getImagen() { return imagen; }
+    public byte[] getSerializada() { return serializada; }
   
     // Setters
     public void setIdHuella(Integer idHuella) { this.idHuella = idHuella; }
     public void setIdAlumno(Integer idAlumno) { this.idAlumno = idAlumno; }
-    public void setImagenHuella(byte[] huella) { this.imagenHuella = huella; }
+    public void setSerializada(byte[] huella) { this.serializada = huella; }
+
 }
