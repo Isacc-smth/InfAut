7d72fdc7d6ee06458bf89e5b918bcb852b4e0dc0
Isaac Feltes <isaacfg2812@gmail.com>
2025-10-04 12:43:12 -0300

    feat: Interfaz para configurar conexiones a la BD

--- src/main/java/ctn/infaut/connection/Conexion.java
+++ src/main/java/ctn/infaut/connection/Conexion.java
@@ -15,42 +15,57 @@ public class Conexion {
     private static HikariDataSource ds;
     private static HikariConfig config = new HikariConfig();
 
-    private static String url ="jdbc:mysql://localhost:3306/isaacfeltes?currentSchema=infaut";
+    private static String url = "jdbc:mysql://localhost:3306/isaacfeltes";
     private static String user = "postgres";
     private static String password = "SpookyKacho";
-    private Conexion() {};
+
+    private Conexion() {}
 
     static {
+        cargarConfigDeArchivo();   
+        reconfigurarDataSource();
+    }
+
+    private static void cargarConfigDeArchivo() {
         try (FileInputStream db = new FileInputStream("db.properties")) {
             Properties props = new Properties();
             props.load(db);
 
-            url = props.getProperty("db.url");
-            user = props.getProperty("db.user");
-            password = props.getProperty("db.password");
+            url = props.getProperty("db.url", url);
+            user = props.getProperty("db.user", user);
+            password = props.getProperty("db.password", password);
 
         } catch (IOException ie) {
-            System.out.println("Usando config default... \n");
+            System.out.println("Usando config default...");
             System.out.println("url: " + url);
             System.out.println("user: " + user);
             System.out.println("password: " + password);
         }
+    }
 
+    private static void reconfigurarDataSource() {
         config.setJdbcUrl(url);
         config.setUsername(user);
         config.setPassword(password);
+
+        if (ds != null && !ds.isClosed()) {
+            ds.close();
+        }
         ds = new HikariDataSource(config);
     }
 
     /**
-     * Guarda la configuracion puesta por el usuario mediante {@link ctn.infaut.ConfigurarDB}
+     * Guarda la configuración en archivo y reconfigura el DataSource.
      *
-     * @param url en formato JDBC
-     * @param user nombre de usuario
-     * @param password contraseña de la base de datos
-     *
-     * */
-    public static void guardarConfig(String url, String user, String password) {
+     * @param newUrl una combinacion entre el puerto, host y el nombre de la BD
+     * @param newUser el nombre del usuario en la BD (se asume que tiene los permisos necesarios)
+     * @param newPassword la contraseña del usuario (se sugiere que tenga una)
+     */
+    public static void guardarConfig(String newUrl, String newUser, String newPassword) {
+        url = newUrl;
+        user = newUser;
+        password = newPassword;
+
         Properties props = new Properties();
         props.setProperty("db.url", url);
         props.setProperty("db.user", user);
@@ -58,26 +73,15 @@ public class Conexion {
 
         try (OutputStream os = new FileOutputStream("db.properties")) {
             props.store(os, "Configuracion puesta por el usuario");
-
-            config.setJdbcUrl(url);
-            config.setUsername(user);
-            config.setPassword(password);
-
-            ds = new HikariDataSource(config);
         } catch (IOException e) {
-            System.err.println("Ocurrio un eror al configurar la conexion: " + e.getMessage());
+            System.err.println("Error al guardar config: " + e.getMessage());
         }
+
+        reconfigurarDataSource();
     }
 
-
-    /**
-     * Metodo usado internamente para obtener conexion a la base de datos
-     *
-     * @return la conexion a la base de datos
-     * @throws SQLException si falla la conexion
-     * */
     public static Connection getCon() throws SQLException {
-        System.out.println(url);
+        System.out.println("Usando URL: " + url);
         return ds.getConnection();
     }
 }
